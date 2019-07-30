d6a4394c70fea9188c2a2dd50d45c297237bc561
wuxingle <1546752823@qq.com>
2019-07-30 17:50:53 +0800

    概率增加

--- stock-build.sh
+++ stock-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /usr/local/bin/docker-compose -f basic-compose.yml down --rmi all
 
-/usr/local/bin/docker-compose -f healthcare-compose.yml down --rmi all
+/usr/local/bin/docker-compose -f stock-compose.yml down --rmi all
 
 /usr/local/bin/docker-compose -f basic-compose.yml up -d
 
-/usr/local/bin/docker-compose -f healthcare-compose.yml up -d
+/usr/local/bin/docker-compose -f stock-compose.yml up -d
